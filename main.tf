434a37e31c8fa6844bf147ce1b80764986ff72da
Your Name <yourgithubemail@example.com>
2025-09-25 12:57:56 +0530

    Task3: Terraform Docker container + logs

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+# On Linux / macOS this empty provider is fine (uses unix socket)
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "task3-nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
+
